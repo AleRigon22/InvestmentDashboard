64117ee3be0ae6ca96a7867ab12acfd5083c3881
AleRigon22 <alerigon6@gmail.com>
2025-07-29 16:28:13 +0200

    Correzione index.html

--- postbuild.js
+++ postbuild.js
@@ -6,10 +6,10 @@ const isWindows = os.platform() === 'win32';
 try {
   if (isWindows) {
     execSync('xcopy shared dist\\shared /E /I /Y', { stdio: 'inherit' });
-    execSync('xcopy dist dist_public /E /I /Y', { stdio: 'inherit' });
+    execSync('xcopy dist\\public dist_public /E /I /Y', { stdio: 'inherit' });
   } else {
     execSync('cp -R shared dist/shared', { stdio: 'inherit' });
-    execSync('cp -R dist dist_public', { stdio: 'inherit' });
+    execSync('cp -R dist/public dist_public', { stdio: 'inherit' });
   }
 } catch (e) {
   console.error('Errore nel postbuild:', e);
